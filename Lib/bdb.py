92b3ee6aa396a169a9fa73baec794b189e438e3b
Daniel Hahler <git@thequod.de>
2019-11-15 06:22:49 +0100

    test: _via_set_trace_frame

--- Lib/bdb.py
+++ Lib/bdb.py
@@ -199,9 +199,10 @@ class Bdb:
 
     def stop_here(self, frame):
         "Return True if frame is below the starting frame in the stack."
-        if (self.stopframe is None
-                and self.returnframes is None
-                and self.stoplineno == 0):  # TEST: via set_step
+        if (
+            self.stopframe is None
+            and getattr(self, "_via_set_trace_frame", frame) == frame
+        ):
             return True
         if self.skip and \
                self.is_skipped_module(frame.f_globals.get('__name__')):
@@ -327,6 +328,7 @@ class Bdb:
         """
         if frame is None:
             frame = sys._getframe().f_back
+        self._via_set_trace_frame = frame
         self.reset()
         while frame:
             frame.f_trace = self.trace_dispatch
